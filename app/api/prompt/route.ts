615d35dc8f71f9a6f9e1901ad4a293b49453d59e
robinmuhia <muhiarobinonyancha@gmail.com>
2023-05-15 12:46:28 +0300

    added get prompts

---
+++ app/api/prompt/route.ts
@@ -0,0 +1,12 @@
+import { connectToDB } from "utils/database";
+import Prompt from "models/prompt";
+
+export const GET = async (req: Request) => {
+  try {
+    await connectToDB();
+    const prompts = await Prompt.find({}).populate("creator");
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to get prompts", { status: 500 });
+  }
+};
--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,7 +1,68 @@
-import React from "react";
+"use client";
+
+import { useState, useEffect } from "react";
+import Promptcard from "./Promptcard";
+
+interface creator {
+  image: string;
+  username: string;
+  email: string;
+}
+
+interface post {
+  _id: string;
+  prompt: string;
+  tag: string;
+  creator: creator;
+}
+interface props {
+  data: Array<post>;
+  handleTagClick: () => void;
+}
+
+const PromptCardList = ({ data, handleTagClick }: props) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {data.map((post: post) => (
+        <Promptcard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+    </div>
+  );
+};
 
 const Feed = () => {
-  return <div>Feed</div>;
+  const [searchText, setSearchText] = useState("");
+  const [posts, setPosts] = useState([]);
+  const handleSearchChange = (e: React.FormEvent<HTMLInputElement>) => {};
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch("/api/prompt");
+      const data = await response.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <section className="feed">
+      <form className="relative w-full next-center">
+        <input
+          type="text"
+          placeholder="Search for a tag or username"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className="search_input peer"
+        />
+      </form>
+      <PromptCardList data={posts} handleTagClick={() => {}} />
+    </section>
+  );
 };
 
 export default Feed;
